13448f1f98760137abd6f3f24b8b02741820508d
Clemente Solorio <37494038+clxmente@users.noreply.github.com>
2018-06-12 18:10:44 -0700

    fixed invite links since it's a new bot

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ async def pineapplerole(ctx):
 
 @bot.command(pass_context=True)
 async def help(ctx):
-    embed = discord.Embed(title='Help Command', description="**Invite Link:** https://discordapp.com/api/oauth2/authorize?client_id=451978077745184785&permissions=8&scope=bot", color = 0xf6d025)
+    embed = discord.Embed(title='Help Command', description="**Invite Link:** https://discordapp.com/api/oauth2/authorize?client_id=456247418288209922&permissions=8&scope=bot", color = 0xf6d025)
     embed.add_field(name="StaffVote", value = "**Description:** Sends an embed to vote for staff position through reacting with an upvote or downvote.\n**Permission Required:** Administrator\n**Arguments:** `member`\n```!staffvote Eric```", inline=False)
     embed.add_field(name="Channelid", value = "**Description:** Sends the channel id of the current channel.\n**Permission Required:** None\n**Arguments:** None\n```!channelid```", inline=False)
     embed.add_field(name="Mute/Unmute", value = "**Description:** Mute/Unmute a user.\n**Permission Required:** Manage Roles\n**Arguments:** `user` `reason`\n```[!mute | !unmute] @user this is a reason```", inline=False)
@@ -115,6 +115,7 @@ async def help(ctx):
     embed.add_field(name="Clear", value = "**Description:** Clears messages from a channel. Can only delete messages in the range of [2, 100]\n**Permission Required:** Administrator\n**Arguments:** `integer`\n```!clear 50```", inline=False)
     embed.add_field(name="Create Role", value = "**Description:** Creates a new role and assigns a random color to it.\n**Permission Required:** Manage Roles\n**Arguments:** Role Name\n```!cr [role name]```", inline=False)
     embed.add_field(name="Edit Role", value = "**Description:** Takes an existing role and assigns a random color to it.\n**Permission Required:** Manage Roles\n**Arguments:** Role Name\n```!er [role name]```", inline=False)
+    embed.add_field(name="Spam", value = "**Description:** Spams a user with a message of your choice.\n**Permission Required:** Ban Members\n**Arguments:** Role Name\n```!spam @user [int][message]```", inline=False)
     embed.add_field(name="hmmcount", value = "**Description:** Shows a count of how many times Pineapple has replied with a thonk.\n**Permission Required** None\n**Arguments:** None\n```hmmcount```", inline=False)
     embed.add_field(name="Duck", value = "**Description:** Loads a random picture of a duck\n**Permission Required** None\n**Arguments:** None\n```!duck```", inline=False)
     embed.add_field(name="Dog", value = "**Description:** Loads a random picture of a dog\n**Permission Required** None\n**Arguments:** None\n```!dog```", inline=False)
@@ -134,7 +135,7 @@ async def pineapple(ctx):
     embed.set_author(name="PineappleBot", icon_url="https://cdn.discordapp.com/attachments/430563327907725312/454868030854397952/PineappleBot.png")
     embed.set_footer(text="PineappleBot | Created by clemente")
 
-    embed.add_field(name="Invite Link:", value="[Invite PineappleBot to your server](https://discordapp.com/api/oauth2/authorize?client_id=451978077745184785&permissions=8&scope=bot)")
+    embed.add_field(name="Invite Link:", value="[Invite PineappleBot to your server](https://discordapp.com/api/oauth2/authorize?client_id=456247418288209922&permissions=8&scope=bot)")
     embed.add_field(name="What is PineappleBot?", value="PineappleBot is a general purpose moderation bot built on the discord.py library. The bot began as a fun project and will continue to have updates pushed out as I learn more.")
     embed.add_field(name="Need help on how to use it?", value="You can check the help command by doing \n\n``!help`` \n\n Updates will constantly be pushed out with more features and new commands.")
     embed.add_field(name="<:TrackrrTwitter:452765661484285952>", value="Follow me on twitter\n[@clxmente](https://twitter.com/clxmente)", inline=True)
@@ -177,7 +178,6 @@ async def trackrr(ctx):
     embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/452763485743349761/452763574503211008/trackrr.png")
     embed.set_author(name="Trackrr Music Search", icon_url="https://cdn.discordapp.com/attachments/452763485743349761/452763575878942720/TrackrrLogo.png")
     embed.set_footer(text="Trackrr Music Search | Created by exofeel")
-
     embed.add_field(name="Invite Link:", value="[Invite Trackrr to your server](https://discordapp.com/api/oauth2/authorize?client_id=449305030802145280&permissions=8&scope=bot)")
     embed.add_field(name="What is Trackrr?", value="Trackrr is a discord bot that searches through the most important streaming websites and find the song that you have asked.")
     embed.add_field(name="What services does this search through?", value="Currently, Trackrr searches through **Spotify, Apple Music, Soundcloud and YouTube**. With other streaming websites coming soon. (Depending on the websites API)")
