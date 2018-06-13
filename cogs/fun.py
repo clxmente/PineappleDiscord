f2776bc7fe6d5ed271f7aa69ffbeaeb59ab6fef6
Clemente Solorio <37494038+clxmente@users.noreply.github.com>
2018-06-12 18:11:15 -0700

    A bit more error handling and minor tweaking

--- cogs/fun.py
+++ cogs/fun.py
@@ -18,7 +18,7 @@ class Fun(object):
             await self.bot.say("Only the goat Padraig can use this command.")
 
     @commands.command(pass_context=True)
-    @commands.has_permissions(administrator=True)
+    @commands.has_permissions(ban_members=True)
     async def spam(self, ctx, user: discord.Member, amount: int, *, content):
         messages = 0 
         while messages < amount:
@@ -26,11 +26,18 @@ class Fun(object):
             messages = messages + 1
             await asyncio.sleep(0.2)
 
+        # example of command is '!spam @user 10 wake up'
+        # this will make the bot ping the user 10 times and say wake up.
+
     @spam.error
     async def spam_error(self, error, ctx):
         if isinstance(error, discord.ext.commands.CheckFailure):
             userID = (ctx.message.author.id)
             await self.bot.send_message(ctx.message.author,"<@%s>: **You don't have permission to perform this action**" % (userID))
+            await self.bot.delete_message(ctx.message)
+        
+        elif isinstance(error, discord.ext.commands.MissingRequiredArgument):
+            await self.bot.say('Missing Required Argument. ```!spam @user 10 wake up```')
             await self.bot.delete_message(ctx.message)
 
 
